35d279d9731ba3401471547b4afd1de259a257db
mixteitei <jittoshiteruyo@gmail.com>
2021-10-27 18:33:53 +0000

    [add_flash_message]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,23 @@
 class BooksController < ApplicationController
+  before_action :baria_book, only: [:edit, :update, :destroy]
+
 
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id), notice:"You have created book successfully."
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def show
@@ -19,12 +27,22 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice:"You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
@@ -33,5 +51,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
+  
+  def baria_book
+    unless Book.find(params[:id]).user_id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,35 @@
 class UsersController < ApplicationController
+  before_action :baria_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
+    @books = Book.where(user_id:@user.id)
   end
   
   def index
+    @book = Book.new
+    @user = current_user
+    @users = User.all
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id), notice:"You have updated user successfully."
+  end
   
   private
   def user_params
-    params.require(:user).permit(:name, :image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
+  def baria_user
+    unless User.find(params[:id]).id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
 end
