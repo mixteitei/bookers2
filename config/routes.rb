83428e10f4fdb36ee3f8e5e780c0904e9b59cbf3
mixteitei <jittoshiteruyo@gmail.com>
2021-11-02 14:13:02 +0000

    [add]favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                       sessions: 'users/sessions' }
-  
+
   root to:'homes#top'
   get "home/about" => "homes#about"
-  
+
   resources :books do
+    resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
   end
-  
+
   resources :users, only:[:index, :show, :edit, :update]
 end
